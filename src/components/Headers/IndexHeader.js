d826785626b72cf17aada51cd90baf1ddd32ba69
ConradMare890317 <Conradmare@gmail.com>
2021-06-25 17:47:28 +0200

    2021/06/25 (pm) Main-Nav, Index changes
    Signed-off-by: ConradMare890317 <Conradmare@gmail.com>

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -1,28 +1,5 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-/*eslint-disable*/
 import React from "react";
-
-// reactstrap components
 import { Container } from "reactstrap";
-
-// core components
 
 function IndexHeader() {
   return (
@@ -53,7 +30,7 @@ function IndexHeader() {
               </div> */}
             </div>
             <h2 className="presentation-subtitle text-center">
-            Portfolio made with Reactstrap.
+            Made with Reactstrap / React - Bootstrap.
             </h2>
           </Container>
         </div>
@@ -65,7 +42,7 @@ function IndexHeader() {
           }}
         />
         <h6 className="category category-absolute">
-          (JSX)
+          JSX - ES 5, 6 & 7 / @Babel
         </h6>
       </div>
     </>
